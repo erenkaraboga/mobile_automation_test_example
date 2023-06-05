f60adc5b52aafbf98c7d5a72586bf8d6425a17a3
erenkaraboga <eren.karaboga@naylalabs.com>
2023-06-05 14:10:40 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mobile_automation_test_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_automation_test_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ integration_test/app_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:mobile_automation_test_example/main.dart' as app;
+void main(){
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Tap the increment button 10 times with 1 second delay',(WidgetTester tester)async{
+  app.main();
+  await tester.pumpAndSettle();
+  final Finder button = find.byIcon(Icons.add);
+  for(var i =0; i<10;i++){
+    await  tester.tap(button);
+    await Future.delayed(const Duration(seconds: 1));
+  }
+  await tester.pumpAndSettle();
+  expect(find.text('10'), findsOneWidget);
+  });
+}
